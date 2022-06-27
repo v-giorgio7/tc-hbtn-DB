cc86db07e9e33d0ed84f2094b4f4f15e22116592
v-giorgio7 <vgiorgio@ciandt.com>
2022-06-27 14:27:51 -0300

    refactor: generalizing function (clean code)

--- 0x01/java_jdbc/ConnectionSQLite.java
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -1,5 +1,3 @@
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,24 +7,30 @@ public class ConnectionSQLite {
     static Connection connection;
 
     public static void main(String[] args) {
-        connection = initConnection();
+        connection = initConnection("org.sqlite.JDBC",
+                "jdbc:sqlite:sqlite_database_2022.db");
 
         try {
             connection.close();
+            System.out.println("Connection to database has been closed");
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    private static Connection initConnection() {
+    private static Connection initConnection(String classPackage, String url) {
         try {
             if (connection == null) {
-                String urlConnection = "jdbc:sqlite:sqlite_database_2022.db";
+                Class.forName(classPackage);
+
+                String urlConnection = url;
+                String databaseName = url.split(":")[1];
 
                 connection = DriverManager.getConnection(urlConnection);
-                System.out.println("Connection to SQLite Database has been established");
+                System.out.println("Connection to " + databaseName + " database has been established");
             }
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
 
